21e186d895c37735d9312ccae1e93e13f582c361
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2025-09-26 09:36:11 +0000

    ajout de la librairie Daisy UI pour le styling et utilisation pour stylisé les pages d'authentification

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,23 +1,38 @@
 import { Form, Link } from "react-router";
 
 export default function ConnexionPage() {
-    return (
-        <div className="w-full h-screen flex flex-col items-center justify-center gap-y-8">
-            <Form action="/api/auth/login" method="post" className="flex flex-col items-center justify-center gap-y-4">
-                <h1>Page de connexion</h1>
-                <div className="flex flex-col">
-                    <label htmlFor="mail-adress">Adresse mail</label>
-                    <input type="email" id="mail-adress" className="border-2 border-black" />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="password">Mot de passe</label>
-                    <input type="password" id="password" className="border-2 border-black" />
-                </div>
-                <button type="submit" className="border-2 p-2">Se connecter</button>
-                <hr className="bg-black w-full" />
-            </Form>
-            <button type="button" className="border-2 p-2">Continuer avec Google</button>
-            <span>Vous n'avez pas de compte ? <Link to='/auth/register' className="text-cyan-700">S'inscrire</Link></span>
-        </div>
-    )    
+  return (
+    <div className="w-full h-screen flex justify-center">
+      <div className="w-76 flex flex-col items-center justify-center gap-y-12">
+        <Form
+          action="/api/auth/login"
+          method="post"
+          className="flex flex-col items-center gap-y-8"
+        >
+          <h1 className="text-3xl font-extrabold">Page de connexion</h1>
+          <div className="flex flex-col">
+            <label htmlFor="mail-adress">Adresse mail</label>
+            <input type="email" id="mail-adress" className="w-76 input" />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" id="password" className="w-76 input" />
+          </div>
+          <button type="submit" className="w-full btn btn-accent">
+            Se connecter
+          </button>
+        </Form>
+        <span className="divider">ou</span>
+        <button type="button" className="w-76 btn btn-primary">
+          Continuer avec Google
+        </button>
+        <span>
+          Vous n'avez pas de compte ?{" "}
+          <Link to="/auth/register" className="text-cyan-700">
+            S'inscrire
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,27 +1,42 @@
 import { Form, Link } from "react-router";
 
 export default function RegisterPage() {
-    return (
-        <div className="w-full h-screen flex flex-col items-center justify-center gap-y-4">
-            <h1>Page d'inscription</h1>
-            <Form action="/api/auth/login" method="post" className="flex flex-col items-center justify-center gap-y-4">
-                <div className="flex flex-col">
-                    <label htmlFor="fullname">Nom complet</label>
-                    <input type="text" name="fullname" id="fullname" className="border-2 border-black" />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="mail-adress">Adresse mail</label>
-                    <input type="email" name="mail-adress" id="mail-adress" className="border-2 border-black" />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="password">Mot de passe</label>
-                    <input type="password" name="password" id="password" className="border-2 border-black" />
-                </div>
-                <button type="submit" className="border-2 p-2">S'inscrire</button>
-                <hr className="bg-black w-full" />
-            </Form>
-            <button type="button" className="border-2 p-2">Continuer avec Google</button>
-            <span>Déja un compte ? <Link to='/auth/login' className="text-cyan-700">Se connecter</Link></span>
-        </div>
-    )
+  return (
+    <div className="w-full h-screen flex justify-center">
+      <div className="w-76 flex flex-col items-center justify-center gap-y-8">
+        <Form
+          action="/api/auth/login"
+          method="post"
+          className="flex flex-col items-center gap-y-8"
+        >
+          <h1 className="text-3xl font-extrabold">Page d'inscription</h1>
+          <div className="flex flex-col">
+            <label htmlFor="mail-adress">Adresse mail</label>
+            <input type="email" id="mail-adress" className="w-76 input" />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" id="password" className="w-76 input" />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Confirmation du mot de passe</label>
+            <input type="password" id="password" className="w-76 input" />
+          </div>
+          <button type="submit" className="w-full btn btn-accent">
+            S'inscrire
+          </button>
+        </Form>
+        <span className="divider">ou</span>
+        <button type="button" className="w-76 btn btn-primary">
+          Continuer avec Google
+        </button>
+        <span>
+          Vous avez déjà un compte ?{" "}
+          <Link to="/auth/login" className="text-cyan-700">
+            Se connecter
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
 }
