478804317d0bb217786eae167acaf57c8be95b76
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2025-09-27 19:47:04 +0000

    mise en commentaires des parties non utilisées pour éviter l'échec du déploiement

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
-import { createBrowserRouter, redirect } from "react-router"
+import { createBrowserRouter } from "react-router"
 import ConnexionPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import HomePage from "./pages/HomePage"
 import { TransactionPage } from "./pages/transactionPage"
 import NotFoundPage from "./pages/404"
 import ForgottenPasswordPage from "./pages/PasswordResetPage"
-import type { LoginRequest, RegisterRequest } from "./lib/service/dto"
-import { BackService } from "./lib/backendFetch"
+// import type { LoginRequest, RegisterRequest } from "./lib/service/dto"
+// import { BackService } from "./lib/backendFetch"
 
 
 export const router = createBrowserRouter([
@@ -21,31 +21,31 @@ export const router = createBrowserRouter([
         {
             path: 'login',
             Component: ConnexionPage,  
-            action: async ({ request }) => {
-                const data = await request.formData()
-                const loginData : LoginRequest = {
-                    'email': data.get('mail-address')!.toString(),
-                    'password': data.get('password')!.toString()
-                }
+            // action: async ({ request }) => {
+            //     const data = await request.formData()
+            //     const loginData : LoginRequest = {
+            //         'email': data.get('mail-address')!.toString(),
+            //         'password': data.get('password')!.toString()
+            //     }
 
-                // const response = await BackService.post<LoginRequest>('/api/login', loginData)
-                // redirect("/")
-            } 
+            //     // const response = await BackService.post<LoginRequest>('/api/login', loginData)
+            //     // redirect("/")
+            // } 
         },
         {
             path: 'register',
             Component: RegisterPage,
-            action: async ({ request }) => {
-                const data = await request.formData()
-                const registeringData : RegisterRequest = {
-                    'name': data.get('fullname')!.toString(),
-                    'email': data.get('mail-address')!.toString(),
-                    'password': data.get('password')!.toString()
-                }
+            // action: async ({ request }) => {
+            //     const data = await request.formData()
+            //     const registeringData : RegisterRequest = {
+            //         'name': data.get('fullname')!.toString(),
+            //         'email': data.get('mail-address')!.toString(),
+            //         'password': data.get('password')!.toString()
+            //     }
 
-                // const response = await BackService.post<RegisterRequest>('/api/register', registeringData)
-                // redirect("/")
-            }
+            //     // const response = await BackService.post<RegisterRequest>('/api/register', registeringData)
+            //     // redirect("/")
+            // }
         },
         {
             path: 'reset-password',
