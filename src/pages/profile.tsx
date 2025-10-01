36778eefe9b519b4f51d3458629b94b64d241e75
Cheikhouna Diop <dcheikhouna5@gmail.com>
2025-10-01 23:11:26 +0000

    fix: correction conflits

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -114,11 +114,7 @@ const Profile: React.FC = () => {
     <div className="w-full h-screen flex">
       <SideBar />
 
-<<<<<<< HEAD
-      <div className="flex-1 p-6 space-y-6">
-=======
       <div className="flex-1 p-6 space-y-6 overflow-auto">
->>>>>>> mohamed-doucoure
         <div className="flex items-center justify-between">
           <div>
             <h1 className="text-2xl font-bold text-gray-900">Paramètres</h1>
